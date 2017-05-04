0204fd95fe7efca1b4934f28c4ef76f7ea671620
Mikito <mikito@pc>
2017-05-04 20:01:00 +0800

    Signed-off-by: Mikito <mikito@pc>

--- src/websiteM/mainclass.java
+++ src/websiteM/mainclass.java
@@ -4,7 +4,7 @@ public class mainclass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world0");
+		System.out.println("Hello world..............");
 
 	}
 
