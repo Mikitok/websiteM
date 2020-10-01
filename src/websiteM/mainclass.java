74374ac7d226d5167d5e94fa7bd5701594305b60
Shaurya-567 <55078046+Shaurya-567@users.noreply.github.com>
2020-10-01 22:15:25 +0530

    Update mainclass.java

--- src/websiteM/mainclass.java
+++ src/websiteM/mainclass.java
@@ -1,10 +1,8 @@
 package websiteM;
 
 public class mainclass {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello world");
+    public static void main(String[] args) {
+		System.out.println("Hello world");// UnderStand to print but next print new line if we write only System.out.print() show next statement are write with same line
 
 	}
 
